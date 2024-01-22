b0e786a13ac000f8401edb983417504a57852ffb
ILIANASEMENOVA <129002657+ILIANASEMENOVA@users.noreply.github.com>
2024-01-22 20:09:00 +0200

    summary

--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -7,6 +7,9 @@ import {
   updateContact,
 } from "../controllers/contactsControllers.js";
 
+import validateBody from "../helpers/validateBody.js";
+import schemas from "../schemas/contactsSchemas.js";
+
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", getAllContacts);
@@ -15,8 +18,16 @@ contactsRouter.get("/:id", getContactById);
 
 contactsRouter.delete("/:id", deleteContact);
 
-contactsRouter.post("/", createContact);
+contactsRouter.post(
+  "/",
+  validateBody(schemas.createContactSchema),
+  createContact
+);
 
-contactsRouter.put("/:id", updateContact);
+contactsRouter.put(
+  "/:id",
+  validateBody(schemas.updateContactSchema),
+  updateContact
+);
 
 export default contactsRouter;
